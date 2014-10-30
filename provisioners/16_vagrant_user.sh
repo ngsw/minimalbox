655dbcaa35c810a955a7168262a7d2579dcbe946
ngsw <ngsw@ngsw.jp>
2014-10-30 23:44:20 +0900

    Fix 'curl 301 -> 400 bad request'     'https://raw.github.com --301--> https://raw.githubusercontent.com'

--- provisioners/16_vagrant_user.sh
+++ provisioners/16_vagrant_user.sh
@@ -1,6 +1,6 @@
 mkdir /home/vagrant/.ssh
 chmod 700 /home/vagrant/.ssh
 cd /home/vagrant/.ssh
-curl -L -o authorized_keys https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub
+curl  -kL -o authorized_keys https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub
 chown -R vagrant:vagrant /home/vagrant/.ssh
 chmod 600 /home/vagrant/.ssh/authorized_keys
