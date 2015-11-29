c83a1bd72012ead8c3ab423863a88c412dd4e441
ngsw <ngsw@ngsw.jp>
2015-11-29 15:46:50 +0900

    Exclude `nkf' from the yum package list

--- provisioners/06_yum_install_remove_chkconfig.sh
+++ provisioners/06_yum_install_remove_chkconfig.sh
@@ -1,4 +1,4 @@
-YUMPKG_INSTALL='ntp vim-enhanced nkf sysstat yum-cron yum-utils cronie-noanacron bind-utils telnet.x86_64 xinetd lsof man traceroute strace git wget tree tcpdump kernel-devel kernel-headers perl make gcc bzip2'
+YUMPKG_INSTALL='ntp vim-enhanced sysstat yum-cron yum-utils cronie-noanacron bind-utils telnet.x86_64 xinetd lsof man traceroute strace git wget tree tcpdump kernel-devel kernel-headers perl make gcc bzip2'
 YUMPKG_EPEL_INSTALL='dkms'
 YUMPKG_REMOVE='cronie-anacron'
 yum install -y ${YUMPKG_INSTALL}
